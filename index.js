ac00259e45c078caa79fcf60c7105844e64a0ff4
Emre Unal <emreunal99@gmail.com>
2015-09-26 03:11:46 +0300

    Make Compass plugin a separate object, lazy init options, make output file name to be configurable

--- index.js
+++ index.js
@@ -3,52 +3,67 @@ var merge = require('lodash-node/modern/objects/merge');
 var compileCompass = require('broccoli-compass-compiler');
 var renameFiles = require('broccoli-rename-files');
 
-module.exports = {
-  name: 'ember-cli-compass-compiler',
-  included: function(app) {
-    this._super.included.apply(this, arguments);
-    var projectName = app.project.name();
+var removeLeadingSlash = function(string) {
+  return (string.length > 0 && string[0] === '/') ? string.slice(1) : string;
+};
 
-    app.registry.add('css', {
-      name: 'ember-cli-compass-compiler',
-      ext: ['scss', 'sass'],
-      toTree: function(tree, inputPath, outputPath) {
-        if (inputPath[0] === '/') {
-          inputPath = inputPath.slice(1);
-        }
+function CompassCompilerPlugin(optionsFn) {
+  this.name = 'ember-cli-compass-compiler';
+  this.ext = ['scss', 'sass'];
+  this.optionsFn = optionsFn;
+}
 
-        if (outputPath[0] === '/') {
-          outputPath = outputPath.slice(1);
-        }
+CompassCompilerPlugin.prototype.toTree = function(tree, inputPath, outputPath, inputOptions) {
 
-        var options = app.options.compassOptions || {};
-        var defaultOptions = {
-          outputStyle: 'compressed',
-          compassCommand: 'compass',
-          importPath: [],
-          getCssDir: function(outputDir) {
-            return path.join(outputDir, outputPath);
-          }
-        };
-        var compassOptions = merge(defaultOptions, options);
+  // Normalize paths
+  inputPath = removeLeadingSlash(inputPath);
+  outputPath = removeLeadingSlash(outputPath);
 
-        var inputTrees = [inputPath];
-        if (compassOptions.importPath.length > 0) {
-          inputTrees = inputTrees.concat(compassOptions.importPath);
-        }
+  // Merge default options with supplied options
+  var defaultOptions = {
+    outputStyle: 'compressed',
+    compassCommand: 'compass',
+    importPath: [],
+    getCssDir: function(outputDir) {
+      return path.join(outputDir, outputPath);
+    }
+  };
+  var compassOptions = merge({}, defaultOptions, this.optionsFn(), inputOptions);
+  var projectName = compassOptions.outputFile;
+  delete compassOptions.outputFile;
 
-        var compassTree = compileCompass(inputTrees, compassOptions);
-        var outputTree = renameFiles(compassTree, {
-          transformFilename: function(filename, basename, extname) {
-            if (basename === 'app' && extname === '.css') {
-              return filename.replace(basename, projectName);
-            }
-            return filename;
-          }
-        });
+  // Watch importPath directories
+  var inputTrees = [inputPath];
+  if (compassOptions.importPath.length > 0) {
+    inputTrees = inputTrees.concat(compassOptions.importPath);
+  }
 
-        return outputTree;
+  // Compile and rename app.css => project-name.scss
+  var compassTree = compileCompass(inputTrees, compassOptions);
+  var outputTree = renameFiles(compassTree, {
+    transformFilename: function(filename, basename, extname) {
+      if (basename === 'app' && extname === '.css') {
+        return filename.replace(basename, projectName);
       }
-    });
+      return filename;
+    }
+  });
+
+  return outputTree;
+};
+
+module.exports = {
+  name: 'Ember CLI Compass Compiler',
+
+  included: function(app) {
+    this.app = app;
+    this._super.included.apply(this, arguments);
+    app.registry.add('css', new CompassCompilerPlugin(this.compassOptions.bind(this)));
+  },
+
+  compassOptions: function () {
+    var options = (this.app && this.app.options.compassOptions) || {};
+    options.outputFile = options.outputFile || this.project.name() + '.css';
+    return options;
   }
 };
